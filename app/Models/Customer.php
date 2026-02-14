0bc368fb9b9b6c5d10698d9754a051aa9ed530c0
mana007777 <e7ifubbb@gmail.com>
2026-02-14 23:34:17 +0300

    migrations part 2

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -6,5 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-    //
+        protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'phone_number',
+        'driver_license_number',
+        'date_of_birth',
+        'membership_level',
+        'points',
+    ];
+
+        public function rentals()
+    {
+        return $this->hasMany(Rental::class);
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany(CarReview::class);
+    }
+
+    public function reservations()
+    {
+        return $this->hasMany(CarReservation::class);
+    }
 }
--- database/migrations/2026_02_14_202254_create_cars_table.php
+++ database/migrations/2026_02_14_202254_create_cars_table.php
@@ -13,6 +13,18 @@ return new class extends Migration
     {
         Schema::create('cars', function (Blueprint $table) {
             $table->id();
+            $table->string('make');
+            $table->string('model');
+            $table->year('year');
+            $table->string('vin')->unique();
+            $table->string('license_plate')->unique();
+            $table->string('color')->nullable();
+            $table->integer('mileage');
+            $table->enum('status', ['Available', 'Rented', 'Maintenance', 'Reserved'])->default('Available');
+            $table->decimal('rental_rate', 10, 2);
+            $table->foreignId('category_id')->nullable()->constrained('vehicle_categories')->nullOnDelete();
+            $table->foreignId('insurance_id')->nullable()->constrained('insurances')->nullOnDelete();
+            $table->foreignId('branch_id')->nullable()->constrained('branches')->nullOnDelete();
             $table->timestamps();
         });
     }
