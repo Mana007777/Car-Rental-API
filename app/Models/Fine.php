6935f07d671cddb95ae4c3906b06c46dd022efc4
mana007777 <e7ifubbb@gmail.com>
2026-03-16 17:48:55 +0300

    Fine Model

--- app/Models/Fine.php
+++ app/Models/Fine.php
@@ -6,9 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Fine extends Model
 {
+    protected $fillable = [
+        'rental_id',
+        'amount',
+        'reason',
+        'paid',
+        'fine_date',
+    ];
+
+    protected $casts = [
+        'paid' => 'boolean',
+        'fine_date' => 'date',
+        'amount' => 'decimal:2',
+    ];
+
     public function rental()
     {
         return $this->belongsTo(Rental::class);
     }
 }
-    
