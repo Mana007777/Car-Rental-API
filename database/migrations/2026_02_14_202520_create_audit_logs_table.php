a434dafdf38f706db73b232685a7c0f69f3f201b
mana007777 <e7ifubbb@gmail.com>
2026-03-16 17:54:51 +0300

    Update migration for audit log

--- database/migrations/2026_02_14_202520_create_audit_logs_table.php
+++ database/migrations/2026_02_14_202520_create_audit_logs_table.php
@@ -6,25 +6,22 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('audit_logs', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->nullable(); // could be employee or admin
-            $table->string('action');
+            $table->foreignId('user_id')->nullable()->constrained('users')->nullOnDelete();
+            $table->string('action'); 
             $table->string('table_name');
-            $table->integer('record_id')->nullable();
+            $table->unsignedBigInteger('record_id')->nullable();
+            $table->text('description')->nullable(); 
+            $table->json('old_values')->nullable(); 
+            $table->json('new_values')->nullable(); 
             $table->timestamp('action_date')->useCurrent();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('audit_logs');
