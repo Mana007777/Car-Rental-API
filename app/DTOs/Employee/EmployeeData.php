646edf8647b664a31537508dc3f9b4f00c1c3beb
mana007777 <e7ifubbb@gmail.com>
2026-03-29 10:57:55 +0300

    fixing

--- app/DTOs/Employee/EmployeeData.php
+++ app/DTOs/Employee/EmployeeData.php
@@ -8,12 +8,14 @@ class EmployeeData
 {
     public function __construct(
         public readonly array $user,
+        public readonly array $employee,
     ) {}
 
     public static function fromRequest(FormRequest $request): self
     {
         return new self(
             user: $request->userData(),
+            employee: $request->employeeData(), 
         );
     }
 }
