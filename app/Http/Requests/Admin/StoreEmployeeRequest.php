2991da92fb1cf30da3ad0a36744ca17b15ce4999
mana007777 <e7ifubbb@gmail.com>
2026-03-29 09:36:28 +0300

    fixing

--- app/Http/Requests/Admin/StoreEmployeeRequest.php
+++ app/Http/Requests/Admin/StoreEmployeeRequest.php
@@ -41,16 +41,16 @@ class StoreEmployeeRequest extends FormRequest
     public function employeeData(int $userId): array
     {
         return [
-            'user_id'       => $userId,
-            'first_name'    => $this->first_name,
-            'last_name'     => $this->last_name,
-            'email'         => $this->email,
-            'phone_number'  => $this->phone_number,
-            'position'      => ucfirst($this->role),
-            'branch_id'     => $this->branch_id,
-            'role'          => $this->role,
-            'hire_date'     => now(),
-            'salary'        => $this->salary,
+            'user_id'      => $userId,
+            'first_name'   => $this->first_name,
+            'last_name'    => $this->last_name,
+            'email'        => $this->email,
+            'phone_number' => $this->phone_number,
+            'position'     => ucfirst($this->role),
+            'password'     => Hash::make($this->password),
+            'branch_id'    => $this->branch_id,
+            'hire_date'    => now(),
+            'salary'       => $this->salary,
         ];
     }
 
